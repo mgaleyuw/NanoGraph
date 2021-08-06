98db46e6b690a9981450b3b9ab5094f9d7941f67
mgaleyuw <86376251+mgaleyuw@users.noreply.github.com>
2021-08-06 15:56:59 -0700

    update
    Updated to make functional

--- R/plottingAndStats.R
+++ R/plottingAndStats.R
@@ -6,7 +6,7 @@
 #'
 #'@param infile Path to the input file
 #'@param qualityCutoff reads with quality scores >= to this cutoff are kept
-#'@return a DataFame of the infile
+#'@return a DataFrame of the infile
 #'@export
 loadTable <- function(infile, qualityCutoff=0){
   df <- utils::read.table(infile, sep="\t", header=TRUE, row.names=1)
@@ -17,15 +17,16 @@ loadTable <- function(infile, qualityCutoff=0){
 #' Calculate read length distribution and coverage then save to .csv
 #' 
 #' Uses metadata DataFrame to calculate read distribution statistics and saves
-#' to file. Not implemented yet: appending results to existing file where row 
+#' to file. Optionally append results to existing file where row 
 #' name = sample name.
 #' 
 #' @param indf DataFrame of read metadata
 #' @param outputname filename (.csv) for export, including path is not saved to
 #' working directory
 #' @param append TRUE or FALSE to append summary statistics to an existing file
+#' @return a vector containing results
 #' @export
-createTextSummary <- function(indf, outputname, append=FALSE){
+createTextSummary <- function(indf, outputname=NA, append=FALSE){
   #basic read length statistics
   quantiles <- summary(indf$Length.of.Read)
   
@@ -39,8 +40,20 @@ createTextSummary <- function(indf, outputname, append=FALSE){
   #The other definition of coverage can't be calculated pre alignment https://www.ecseq.com/support/ngs/how-to-calculate-the-coverage-for-a-sequencing-experiment
   #still need to look into how to use RSamtools on an index file, for this problem and also for filtering out only T-LRS reads.
   
-df <- data.frame("coverage"=coverageAll, "coverageNonN"=coverageNonN, "minRead"=quantiles["Min."], "maxRead"=quantiles["Max."], "meanRead"=quantiles["Mean"], "N25"=quantiles["1st Qu."], "N50"=quantiles["Median"], "N75"=quantiles["3rd Qu."])
-utils::write.csv(df, outputname)
+  if(! is.na(outputname)){
+    writeline=paste(coverageAll, coverageNonN, qunatiles["Min."], quantiles["Max."], quantiles["Mean"], quantiles["1st Qu."], quantiles["Median"], quantiles["3rd Qu."], sep="\t")
+    if(append){
+      write(writeline,file=outputname.txt,append=TRUE)
+    } else {
+      outfile=file(outputname, "w")
+      header=paste("coverage", "coverageNonN", "minRead", "maxRead", "meanRead", "N25", "N50", "N75", sep="\t")
+      writeLines(header, con=outfile)
+      writeLines(writeline, con=outfile)
+      close(outfile)
+    }
+  } else {
+    return( c("coverage"=coverageAll, "coverageNonN"=coverageNonN, "minRead"=quantiles["Min."], "maxRead"=quantiles["Max."], "meanRead"=quantiles["Mean"], "N25"=quantiles["1st Qu."], "N50"=quantiles["Median"], "N75"=quantiles["3rd Qu."]) )
+  }
 }
 
 #'Plot bar chart of read counts or cumulative read KB in bins
@@ -59,22 +72,23 @@ utils::write.csv(df, outputname)
 #'@param outunits unit of width and height, default 'in'
 #'@param plotQual TRUE or FALSE, default FALSE, plot quality distribution as fill
 #'@param qual.bins a vector of quality cutoffs for use with plotQual
+#'@param barColor an r color or color hex code
 #'@return a ggplot2 plot
 #'@export
-plotReadCountDistributions <- function(indf, outname, binNum=50, counts=FALSE, skinnyBin=FALSE, outwidth=8, outheight=7, outunits="in", plotQual=FALSE, qual.bins=c(10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60)) {
+plotReadCountDistributions <- function(indf, outname, binNum=50, counts=FALSE, skinnyBin=FALSE, outwidth=8, outheight=7, outunits="in", plotQual=FALSE, qual.bins=c(10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60), barColor="#8ec462") {
   ggplot2::theme_set(ggplot2::theme_classic(base_size=12, base_family="Avenir"))
   if(counts){
     bw=10000
     if(skinnyBin){
       bw=5000
     }
-    g <- plotCountBases(indf, binNum, plotQual=plotQual, qual.bins=qual.bins, binWidth=bw)
+    g <- plotCountBases(indf, binNum, plotQual=plotQual, qual.bins=qual.bins, binWidth=bw, barColor=barColor)
   } else {
     bw=5000
     if(skinnyBin){
       bw=2500
     }
-    g <- plotCumulativeBases(indf, binNum, plotQual=plotQual, qual.bins=qual.bins, binWidth=bw)
+    g <- plotCumulativeBases(indf, binNum, plotQual=plotQual, qual.bins=qual.bins, binWidth=bw, barColor=barColor)
   }
   if(! is.na(outname)){
     ggplot2::ggsave(outname, dpi=300, width=outwidth, height=outheight, units=outunits)
@@ -95,7 +109,7 @@ plotReadCountDistributions <- function(indf, outname, binNum=50, counts=FALSE, s
 #'@param barColor the name or hex string of a color if not plotting quality
 #'@return a ggplot2 plot
 #'@export
-plotCountBases <- function(df, binNum, plotQual=TRUE, qual.bins=c(10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60), binWidth=8000, barColor="#8ec462"){
+plotCountBases <- function(df, bin.num, plotQual=TRUE, qual.bins=c(10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60), binWidth=8000, barColor="#8ec462"){
   bins <- seq(from=min(df$Length.of.Read), to=max(df$Length.of.Read), length.out=bin.num)
   df$bincode <- .bincode(df$Length.of.Read, bins, right=FALSE)
   if(plotQual==FALSE){
